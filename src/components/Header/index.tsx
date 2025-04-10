094a8b200fd38e0f9cf64e381b6bd9a63435190b
Marcos Pereira <marcos.p.10@hotmail.com>
2025-04-10 00:19:52 -0300

    Adiciona fontes Coolvetica e AGaramond, e implementa o cabeçalho com navegação no componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,34 @@
 "use client"
 import React from 'react';
-import {HeaderProps} from './types';
+import { HeaderProps } from './types';
 import styles from './style.module.scss';
 
 export const Header = (props: HeaderProps): React.JSX.Element => {
     return (
-        <div className={styles.header}>
-            Header
-        </div>
+        <header className={styles.headerContainer}>
+            <nav className={styles.navbarContainer}>
+                <div className={styles.titleContainer}>
+                    <h1>
+                        FELISPO
+                    </h1>
+                </div>
+                <div className={styles.menuContainer}>
+                    <ul>
+                        <li>
+                            <a href="#home">SOBRE</a>
+                        </li>
+                        <li>
+                            <a href="#about">PROJETOS</a>
+                        </li>
+                        <li>
+                            <a href="#services">CURRICULO</a>
+                        </li>
+                        <li>
+                            <a href="#contact">CONTATO</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     )
 }
