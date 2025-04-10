c00747c7bf24ba85c5930167e0e1132519025287
Marcos Pereira <marcos.p.10@hotmail.com>
2025-04-10 08:53:48 -0300

    Remove imports and types related to HeaderProps for simplification

--- src/components/Header/types.ts
+++ src/components/Header/types.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export interface HeaderProps {
-    hiddenMenu?: boolean;
-    children?: React.ReactNode;
-}
+// export interface HeaderProps {
+//     hiddenMenu?: boolean;
+//     children?: React.ReactNode;
+// }
