223d69b074d148b538a05b6b9850c38a2a24bbad
Marcos Pereira <marknotas@outlook.com>
2025-04-16 21:26:32 -0300

    Reorganiza a estrutura do componente Felispo, removendo o comentário e adicionando texto de teste

--- src/containers/home/Felispo/index.tsx
+++ src/containers/home/Felispo/index.tsx
@@ -7,16 +7,6 @@ import styles from './styles.module.scss';
 export const Felispo = (): React.JSX.Element => {
     return (
         <div className={styles.container}>
-            {/* <div className={styles.cavalinhoContainer}>
-                <Image
-                    src="/static/images/cavalinho.gif" // Path to cavalinho.gif
-                    alt="Cavalinho Animation"
-                    width={300} // Adjust width as needed
-                    height={300} // Adjust height as needed
-                    className={styles.cavalinhoImage} // Optional: Add a class for styling
-                />
-            </div> */}
-            teste
             <div className={styles.imageContainer}>
                 <Image
                     src="/static/images/felispo.png"
@@ -26,7 +16,15 @@ export const Felispo = (): React.JSX.Element => {
                     className={styles.image}
                 />
             </div>
-            
+            <div className={styles.cavalinhoContainer}>
+                <Image
+                    src="/static/images/cavalinho.gif" // Path to cavalinho.gif
+                    alt="Cavalinho Animation"
+                    width={300} // Adjust width as needed
+                    height={300} // Adjust height as needed
+                    className={styles.cavalinhoImage} // Optional: Add a class for styling
+                />
+            </div>
         </div>
     )
 }
