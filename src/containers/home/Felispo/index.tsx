26dfee19ecb3299d1a3595a077e29510a6fc77b9
Marcos Pereira <marknotas@outlook.com>
2025-04-23 23:43:13 -0300

    refactor: ajusta estrutura e estilos do componente Felispo, incluindo melhorias no vídeo e na imagem

--- src/containers/home/Felispo/index.tsx
+++ src/containers/home/Felispo/index.tsx
@@ -1,34 +1,36 @@
-"user client"
-import React from 'react';
-// import Image from 'next/image';
-import styles from './styles.module.scss';
-
+"use client";
+import React, { useRef } from "react";
+import Image from "next/image";
+import styles from "./styles.module.scss";
 
 export const Felispo = (): React.JSX.Element => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
     return (
         <div className={styles.container}>
             <div className={styles.videoContainer}>
                 <video
-                    width={500}
-                    height={500}
-                    autoPlay
-                    loop
+                    ref={videoRef}
                     muted
                     playsInline
+                    loop
                     className={styles.video}
+                    autoPlay
                 >
                     <source src="/static/videos/felispo.webm" type="video/webm" />
                 </video>
             </div>
-            {/* <div className={styles.cavalinhoContainer}>
-                <Image
-                    src="/static/images/cavalinho.gif" // Path to cavalinho.gif
-                    alt="Cavalinho Animation"
-                    width={300} // Adjust width as needed
-                    height={300} // Adjust height as needed
-                    className={styles.cavalinhoImage} // Optional: Add a class for styling
-                />
-            </div> */}
+            <div className={styles.cavalinhoContainer}>
+                <div className={styles.cavalinho}>
+                    <Image
+                        src="/static/images/cavalinho.gif" // Path to cavalinho.gif
+                        alt="Cavalinho Animation"
+                        width={300} // Adjust width as needed
+                        height={300} // Adjust height as needed
+                        className={styles.cavalinhoImage} // Optional: Add a class for styling
+                    />
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
